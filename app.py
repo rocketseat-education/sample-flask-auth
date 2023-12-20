801b130c151f9cbade5074e3d9ed2a8d355d6dce
Tamires Santos <tfs.tamsantos@gmail.com>
2023-12-20 13:38:57 -0300

    Criando banco de dados e organizando componentes

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from models.user import User
+from database import db
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
-db = SQLAlchemy(app)
+db.init_app(app)
+#Session <- conexão ativa
 
 @app.route("/hello-world", methods=["GET"])
 def hello_world():
--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
-from app import db
+from database import db
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(db.Model, UserMixin):
   # id (int), username (text), password (text)
   id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(80), nullable=False, unique=True)
