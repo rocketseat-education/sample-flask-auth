df132eaf5d2ab771c2a3252f2674d86843b1881e
Tamires Santos <tfs.tamsantos@gmail.com>
2023-12-21 13:57:27 -0300

    Rotas de Read e Update do usuário

--- app.py
+++ app.py
@@ -55,6 +55,40 @@ def create_user():
 
   return jsonify({"message": "Dados invalidos"}), 400
 
+@app.route('/user/<int:id_user>', methods=["GET"])
+@login_required
+def read_user(id_user):
+  user = User.query.get(id_user)
+
+  if user:
+    return {"username": user.username}
+  
+  return jsonify({"message": "Usuario não encontrado"}), 404
+
+@app.route('/user/<int:id_user>', methods=["PUT"])
+@login_required
+def update_user(id_user):
+  data = request.json
+  user = User.query.get(id_user)
+
+  if user and data.get("password"):
+    user.password = data.get("password")
+    db.session.commit()
+
+    return jsonify({"message": f"Usuário {id_user} atualizado com sucesso"})
+  
+  return jsonify({"message": "Usuario não encontrado"}), 404
+
+@app.route('/user/<int:id_user>', methods=["DELETE"])
+@login_required
+def delete_user(id_user):
+  user = User.query.get(id_user)
+
+  if user:
+    return jsonify({"message": f"Usuário {id_user} deletado com sucesso"})
+  
+  return jsonify({"message": "Usuario não encontrado"}), 404
+
 @app.route("/hello-world", methods=["GET"])
 def hello_world():
   return "Hello world"
