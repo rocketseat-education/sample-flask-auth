22847f9747ebcf426cc01ccb19b3cd256ba61bd2
Tamires Santos <tfs.tamsantos@gmail.com>
2023-12-21 14:07:17 -0300

    Atualizando repositório do Github

--- app.py
+++ app.py
@@ -94,9 +94,5 @@ def delete_user(id_user):
   
   return jsonify({"message": "Usuario não encontrado"}), 404
 
-@app.route("/hello-world", methods=["GET"])
-def hello_world():
-  return "Hello world"
-
 if __name__ == '__main__':
   app.run(debug=True)
