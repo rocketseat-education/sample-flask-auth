9fb75abe259177ddeeee59a503c4dc778933a818
Tamires Santos <tfs.tamsantos@gmail.com>
2023-12-21 13:33:07 -0300

    Criando views de logout e cadastro de usuário

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from flask_login import LoginManager, login_user, current_user
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
@@ -34,6 +34,26 @@ def login():
       return jsonify({"message": "Autenticação realizada com sucesso!"})
 
   return jsonify({"message": "Credenciais inválidas"}), 400
+
+@app.route('/logout', methods=['GET'])
+@login_required
+def logout():
+  logout_user()
+  return jsonify({"message": "Logout realizado com sucesso!"})
+
+@app.route('/user', methods=["POST"])
+def create_user():
+  data = request.json
+  username = data.get("username")
+  password = data.get("password")
+
+  if username and password:
+    user = User(username=username, password=password)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({"message": "Usuario cadastrado com sucesso"})
+
+  return jsonify({"message": "Dados invalidos"}), 400
 
 @app.route("/hello-world", methods=["GET"])
 def hello_world():
