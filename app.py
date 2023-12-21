8cc180ae4d45755ac82ba148c765f2402eb6df6b
Tamires Santos <tfs.tamsantos@gmail.com>
2023-12-21 14:05:07 -0300

    Rotas de Delete de usuário

--- app.py
+++ app.py
@@ -84,7 +84,12 @@ def update_user(id_user):
 def delete_user(id_user):
   user = User.query.get(id_user)
 
+  if id_user == current_user.id:
+    return jsonify({"message": "Deleção não permitida"}), 403
+
   if user:
+    db.session.delete(user)
+    db.session.commit()
     return jsonify({"message": f"Usuário {id_user} deletado com sucesso"})
   
   return jsonify({"message": "Usuario não encontrado"}), 404
