53b0f26c99d370520f10ce26a36f708d81ddeef3
Tamires Santos <tfs.tamsantos@gmail.com>
2023-12-22 13:22:20 -0300

    Utilizando banco de dados MYSQL

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager, login_user, current_user, logout_user, log
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql:///root:admin123@127.0.0.1:3306/flask-crud'
 
 login_manager = LoginManager()
 db.init_app(app)
